37f3cd94136b0a6330f976c925af23396f987144
tmznwnel00 <tmznwnel00@gmail.com>
2026-04-25 20:44:11 +0900

    feat: code for fine-tunig & sampling

--- examples/test_sample.py
+++ examples/test_sample.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from be_great import GReaT
 
 import logging
@@ -6,7 +11,7 @@ from sklearn import datasets
 
 logger = set_logging_level(logging.INFO)
 
-great = GReaT.load_from_dir("iris")
+great = GReaT.load_from_dir("trained/adult_topology")
 
 # Continuous column as start
 # data, target = datasets.load_iris(return_X_y=True)
@@ -17,9 +22,17 @@ great = GReaT.load_from_dir("iris")
 # samples = great.sample(12, device="cpu", k=6)
 
 # Categorical column as start
-samples = great.sample(
-    20, k=5, start_col="target", start_col_dist={"0.0": 0.33, "1.0": 0.33, "2.0": 0.33}
-)
 
-print(samples)
-samples.to_csv("iris_samples.csv")
+os.makedirs("result/adult/topology/", exist_ok=True)
+for i in range(1, 4):
+    samples = great.sample(
+        n_samples=16281, # adult
+        # n_samples=1600, # healthcare
+        # n_samples=8000, # asia
+        device="cuda", 
+        guided_sampling=True,
+        start_col="race" #topology
+        )
+
+    # print(samples)
+    samples.to_csv(f"result/adult/topology/adult_test_topology{i}.csv")
--- examples/test_train.py
+++ examples/test_train.py
@@ -1,26 +1,150 @@
-from be_great import GReaT
-from sklearn import datasets
-
+import sys
+import os
 import logging
+import pandas as pd
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from be_great.great import GReaT
+from sklearn import datasets
 from utils import set_logging_level
+
+# # healthcare data
+# base_dir = os.path.dirname(os.path.abspath(__file__))
+# df = pd.read_csv(os.path.join(base_dir, "healthcare.txt"), sep=" ")
+# train_df = df.iloc[:1600]
+# test_df = df.iloc[1600:]
+# cols = ["A", "C", "D", "H", "I", "O", "T"]
+
+# asia data
+# import bnlearn as bn
+# df = bn.import_example("asia")
+# train_df = df.iloc[:8000]
+# test_df = df.iloc[8000:]
+# cols = ["asia", "tub", "smoke", "lung", "bronc", "either", "xray", "dysp"]
+
+# adult data
+from ucimlrepo import fetch_ucirepo
+adult = fetch_ucirepo(id=2)
+df = adult.data.features.copy()
+df["income"] = adult.data.targets["income"]
+df["income"] = df["income"].str.replace(".", "", regex=False)
+df = df.drop(columns=["fnlwgt", "education-num", "capital-gain", "capital-loss"])
+train_df = df.iloc[:32561]
+test_df = df.iloc[32561:]
+cols = ["age", "workclass", "education", "marital-status", "occupation", "relationship", "race", "sex", "hours-per-week", "native-country", "income"]
+
 
 logger = set_logging_level(logging.INFO)
 
-data = datasets.load_iris(as_frame=True).frame
-print(data.head())
+# adult
+dag_edges = [
+    ("race", "education"),
+    ("race", "occupation"),
+    ("race", "income"),
+    ("age", "education"),
+    ("age", "hours-per-week"),
+    ("age", "workclass"),
+    ("age", "marital-status"),
+    ("age", "occupation"),
+    ("age", "income"),
+    ("native-country", "education"),
+    ("native-country", "hours-per-week"),
+    ("native-country", "marital-status"),
+    ("native-country", "occupation"),
+    ("native-country", "income"),
+    ("sex", "education"),
+    ("sex", "hours-per-week"),
+    ("sex", "marital-status"),
+    ("sex", "relationship"),
+    ("sex", "income"),
+    ("education", "hours-per-week"),
+    ("education", "workclass"),
+    ("education", "marital-status"),
+    ("education", "occupation"),
+    ("education", "income"),
+    ("hours-per-week", "marital-status"),
+    ("hours-per-week", "occupation"),
+    ("hours-per-week", "income"),
+    ("workclass", "marital-status"),
+    ("workclass", "occupation"),
+    ("marital-status", "occupation"),
+    ("marital-status", "relationship"),
+    ("marital-status", "income"),
+    ("occupation", "income"),
+    ("relationship", "income"),
+]
 
-column_names = ["sepal length", "sepal width", "petal length", "petal width", "target"]
-data.columns = column_names
+
+# healthcare
+# dag_edges = [
+#     ("A", "H"),
+#     ("A", "D"),
+#     ("A", "C"),
+#     ("A", "O"),
+#     ("H", "D"),
+#     ("D", "I"),
+#     ("C", "I"),
+#     ("I", "T"),
+#     ("O", "T"),
+# ]
+
+# asia
+# dag_edges = [
+#     ("asia", "tub"),   
+#     ("tub", "either"),
+#     ("smoke", "lung"),
+#     ("smoke", "bronc"),
+#     ("lung", "either"),
+#     ("either", "xray"),
+#     ("either", "dysp"),
+#     ("bronc", "dysp"),
+# ]
 
 great = GReaT(
     "distilgpt2",
-    epochs=50,
-    save_steps=100,
+    epochs=200,
+    save_steps=200000,
     logging_steps=5,
-    experiment_dir="trainer_iris",
+    experiment_dir="trained/trainer_adult_topology_bias",
     # lr_scheduler_type="constant", learning_rate=5e-5
 )
 
-trainer = great.fit(data, column_names=column_names)
+dag_alpha = 1.0 * 1.0
+dag_beta = 0.0
+dag_gamma = 1.0 * 1.0
+dag_bias_learnable = False  # False로 두면 alpha/beta/gamma 고정
 
-great.save("iris")
+trainer = great.fit(
+    train_df,
+    column_names=cols,
+    dag_edges=dag_edges,
+    dag_alpha_init=dag_alpha,
+    dag_beta_init=dag_beta,
+    dag_gamma_init=dag_gamma,
+    dag_bias_learnable=dag_bias_learnable,
+    conditional_col="race", #topology
+    random_conditional_col=False, #topology
+)
+
+dag_bias = getattr(great.model, "dag_attention_bias", None)
+# if dag_bias is not None:
+#     print("\n[DAG Attention Bias Scalars]")
+#     print(f"alpha: {dag_bias.alpha.item():.6f}")
+#     print(f"beta : {dag_bias.beta.item():.6f}")
+#     print(f"gamma: {dag_bias.gamma.item():.6f}")
+
+great.save("trained/adult_topology_bias")
+
+os.makedirs("result/adult/topology/", exist_ok=True)
+for i in range(1, 2):
+    samples = great.sample(
+        n_samples=16281, # adult
+        # n_samples=1600, # healthcare
+        # n_samples=8000, # asia
+        device="cuda", 
+        guided_sampling=True,
+        start_col="race" #topology
+        )
+
+    # print(samples)
+    samples.to_csv(f"result/adult/topology/adult_test_topology_bias{i}.csv")
